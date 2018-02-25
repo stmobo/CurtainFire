256b328a6e664347b601160d1e201cacc32bae42
Sebastian Mobo <stmobo@gmail.com>
2018-02-24 18:23:47 -0600

    Reset starting wave time for normal difficulty

--- waves.py
+++ waves.py
@@ -786,9 +786,9 @@ def reset():
         starting_wave_size = 40
         wave_size_increase = 5
     elif game_data.difficulty == 1:
-        starting_wave_size = 60
+        starting_wave_time = 11
         wave_time_decrease = .5
-        base_wave_size = starting_wave_size
+        starting_wave_size = 60
         wave_size_increase = 5
     elif game_data.difficulty == 2:
         starting_wave_time = 9
